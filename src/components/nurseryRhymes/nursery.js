27b2b217c58218730cbf193a7afed0a18fe18133
= <beckyljones151192@gmail.com>
2021-04-30 14:55:31 +0100

    removed some red rubbish and placed comments for future

--- src/components/nurseryRhymes/nursery.js
+++ src/components/nurseryRhymes/nursery.js
@@ -11,13 +11,16 @@ function Nursery() {
       <Button />
       <br></br>
       <div className="videos">
+        {/* <div className="twinkle"> */}
         <ReactPlayer
           controls
           url="https://www.youtube.com/watch?v=yCjJyiqpAuU"
           width="99%"
           height="200px"
+          // height="450px"
           loop
         />
+        {/* </div> */}
         <br></br>
         <ReactPlayer
           controls
@@ -64,64 +67,6 @@ function Nursery() {
         Courtesy of youtube Sorry if they drive you nuts 😊
       </p>
     </div>
-
-    // {/* <div className="nursery">
-    //   <h1 className="nurseryHeading">🎵 Nursery Rhymes 🎵</h1>
-    //   <Button />
-    //   <br></br>
-    //   {/* <div className="nurseryVideos">
-    //     <ReactPlayer
-    //       controls
-    //       url="https://www.youtube.com/watch?v=yCjJyiqpAuU"
-    //       width="99%"
-    //       height="200px"
-    //       loop
-    //     />
-    //     <br></br>
-    //     <ReactPlayer
-    //       controls
-    //       url="https://www.youtube.com/watch?v=_6HzoUcx3eo"
-    //       width="99%"
-    //       height="200px"
-    //       loop
-    //     />
-    //     <br></br>
-    //     <ReactPlayer
-    //       controls
-    //       url="https://www.youtube.com/watch?v=7otAJa3jui8"
-    //       width="99%"
-    //       height="200px"
-    //       loop
-    //     />
-    //     <br></br>
-    //     <ReactPlayer
-    //       controls
-    //       url="https://www.youtube.com/watch?v=yWirdnSDsV4"
-    //       width="99%"
-    //       height="200px"
-    //       loop
-    //     />
-    //     <br></br>
-    //     <ReactPlayer
-    //       controls
-    //       url="https://www.youtube.com/watch?v=M6LoRZsHMSs"
-    //       width="99%"
-    //       height="200px"
-    //       loop
-    //     />
-    //     <br></br>
-    //     <ReactPlayer
-    //       controls
-    //       url="https://www.youtube.com/watch?v=1GDFa-nEzlg"
-    //       width="99%"
-    //       height="200px"
-    //       loop
-    //     />
-    //   </div> */}
-    //   {/* <div className="text">
-    //     <p>Courtesy of youtube Sorry if they drive you nuts 😊</p>
-    //   </div> */}
-    // </div> */}
   );
 }
 
